972714712eddcd702546c41a4666a3aad5483f97
beyondinfinity2500-ctrl <beyondinfinity2500@gmail.com>
2026-08-08 23:11:08 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',       // پوشه اپ (صفحات اصلی)
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',     // اگر احیاناً پوشه pages داشتید
+    './components/**/*.{js,ts,jsx,tsx,mdx}', // پوشه کامپوننت‌ها
   ],
-  theme: { extend: {}, },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
