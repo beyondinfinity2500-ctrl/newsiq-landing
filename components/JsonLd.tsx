47bc9e294fd1286e57adbbb10d8e4a1e32d8b061
beyondinfinity2500-ctrl <beyondinfinity2500@gmail.com>
2026-08-08 21:39:07 +0330

    Update JsonLd.tsx

--- components/JsonLd.tsx
+++ components/JsonLd.tsx
@@ -1,1 +1,19 @@
 
+export default function JsonLd() {
+  const schemaData = {
+    '@context': 'https://schema.org',
+    '@type': 'NewsMediaOrganization',
+    name: 'NEWSiQ',
+    url: 'https://www.newsiq.top',
+    logo: 'https://www.newsiq.top/logo.svg',
+    description: 'Global micro-news analysis platform providing real-time financial market intelligence.',
+    sameAs: [],
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+    />
+  );
+}
